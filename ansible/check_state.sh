ae5038a10f11624a4415c419f345f39e636a1959
Agafonov Andrey <a.agafonov@smartcat.pro>
2020-05-08 03:26:38 +0300

    qwer

--- ansible/check_state.sh
+++ ansible/check_state.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 chmod 0600 /vagrant/ansible.pem
 all_hosts=(
-ansible
 mdb-afina
 mdb-fobos
 mdb-gera
-mdb-afrodita
-mdb-geya
-mdb-clio
-mdb-erida
-mdb-femida
-mdb-eos
-gluster-ares
-gluster-erot
-cer-appollo
-cer-dionis
-cer-morphius
-haproxy-zefs
-haproxy-aid
-kibana-plutos
-prometheus-poseydon
+# mdb-afrodita
+# mdb-geya
+# mdb-clio
+# mdb-erida
+# mdb-femida
+# mdb-eos
+# gluster-ares
+# gluster-erot
+# cer-appollo
+# cer-dionis
+# cer-morphius
+# haproxy-zefs
+# haproxy-aid
+# kibana-plutos
+# prometheus-poseydon
 )
+status_defence=()
 echo -e "Hostname" "\t" "\t" "SELinux State" "\t" "Firewall open ports" "\t" "Firewall State"
-for target_host in ${all_hosts[*]}
+for target_host in ${!all_hosts[@]}
 do
-  ssh -q -o "StrictHostKeyChecking no" vagrant@${target_host} -i /vagrant/ansible.pem 'sudo  echo -e $(hostname) "\t" "\t" $(/usr/sbin/getenforce) "\t" $(sudo firewall-cmd --zone=public --list-ports) "\t" $(systemctl status firewalld |grep "Active:"| sed -En "s/Active: //p")'
+  array=$( ssh -q -o "StrictHostKeyChecking no" vagrant@${all_hosts[$target_host]} -i /vagrant/ansible.pem 'sudo  echo -e $(hostname)";"$(/usr/sbin/getenforce)";"$(systemctl status firewalld |grep "Active:"| sed -En "s/Active: //p")";"$(sudo firewall-cmd --zone=public --list-ports)' )
+  #echo $array
+  status_defence+=( "$array :" )
+  echo ${status_defence[$target_host]}| column -t -s ";"
   #ssh -q -o "StrictHostKeyChecking no" vagrant@${target_host} -i /vagrant/ansible.pem 'sudo systemctl start firewalld'
 done
+echo ${status_defence[@]}
